0485403b7bcd00ccc0e96399c1c2a8591aadda57
YasCueLav <yaslavcue@gmail.com>
2018-01-07 00:44:30 -0300

    vistas

---
+++ PedirPizzaP/PedirPizzaP/ViewTamanio.swift
@@ -0,0 +1,35 @@
+//
+//  ViewTamanio.swift
+//  PedirPizzaP
+//
+//  Created by Yasmin Cueva Lavezzo on 7/1/18.
+//  Copyright © 2018 Yasmin Cueva Lavezzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewTamanio: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
