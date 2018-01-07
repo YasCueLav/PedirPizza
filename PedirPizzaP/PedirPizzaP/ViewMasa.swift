9d17974166b9832c323adedc6f2e9e8207fca1bb
YasCueLav <yaslavcue@gmail.com>
2018-01-07 02:21:17 -0300

    s

--- PedirPizzaP/PedirPizzaP/ViewMasa.swift
+++ PedirPizzaP/PedirPizzaP/ViewMasa.swift
@@ -8,28 +8,86 @@
 
 import UIKit
 
-class ViewMasa: UIViewController {
+class ViewMasa: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var lblMasa: UITextField!
+    @IBOutlet weak var scroll: UIScrollView!
+    
+    var tam:String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func textFieldDoneEditing(sender:UITextField){
+        sender.resignFirstResponder() //desaparece teclado
     }
-    */
+    @IBAction func backgroundTap (sender:UIControl){
+        lblMasa.resignFirstResponder()
+        scroll.resignFirstResponder()
+    }
+    @IBAction func textFieldDidBeginEditing(textField: UITextField) {
+        var punto: CGPoint
+        punto = CGPointMake(0, textField.frame.origin.y-50)
+        self.scroll.setContentOffset(punto, animated: true)
+    }
+    @IBAction func textFieldDidEndEditing (textField: UITextField) {
+        self.scroll.setContentOffset(CGPointZero, animated: true)
+    }
+    
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let resultado = tamMas()
+        let sigVista = segue.destinationViewController as! ViewMasa
+        sigVista.   =resultado
+    }
+    
+    func tamMas () -> String {
+        let tamanio:String = tam
+        let masa:String
+        print("\(tamanio) \(masa)")
+        return masa
+    }
+}
+
+/*
+class VistaResultadoViewController: UIViewController {
+
+@IBOutlet weak var lblResultado: UILabel!
+
+var idiceIM:Double = 0
+
+override func viewWillAppear(animated: Bool) {
+lblResultado.text = String(idiceIM)
+}
+
+override func viewDidLoad() {
+super.viewDidLoad()
+
+// Do any additional setup after loading the view.
+}
+
+override func didReceiveMemoryWarning() {
+super.didReceiveMemoryWarning()
+// Dispose of any resources that can be recreated.
+}
+
+
+/*
+// MARK: - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+// Get the new view controller using segue.destinationViewController.
+// Pass the selected object to the new view controller.
+}
+*/
 
 }
+
+
+*/
--- PedirPizzaP/PedirPizzaP/ViewTamanio.swift
+++ PedirPizzaP/PedirPizzaP/ViewTamanio.swift
@@ -8,28 +8,47 @@
 
 import UIKit
 
-class ViewTamanio: UIViewController {
+class ViewTamanio: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var lblTam: UITextField!
+    @IBOutlet weak var btnSig: UIButton!
+    @IBOutlet weak var scroll: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func textFieldDoneEditing(sender:UITextField){
+        sender.resignFirstResponder() //desaparece teclado
     }
-    */
+    @IBAction func backgroundTap (sender:UIControl){
+        btnSig.resignFirstResponder()
+        scroll.resignFirstResponder()
+        lblTam.resignFirstResponder()
+    }
+    @IBAction func textFieldDidBeginEditing(textField: UITextField) {
+        var punto: CGPoint
+        punto = CGPointMake(0, textField.frame.origin.y-50)
+        self.scroll.setContentOffset(punto, animated: true)
+    }
+    @IBAction func textFieldDidEndEditing (textField: UITextField) {
+        self.scroll.setContentOffset(CGPointZero, animated: true)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let resultado = tam()
+        let sigVista = segue.destinationViewController as! ViewMasa
+        sigVista.tam=resultado
+    }
+    
+    func tam () -> String {
+        let tamanio:String
+        print("\(tamanio)")
+        return tamanio
+    }
 
 }
